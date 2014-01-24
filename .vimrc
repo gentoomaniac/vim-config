5b97d85a40073f65bc3acdad813f3ac6917faac9
Marco Siebecke <marco@spotify.com>
2014-01-24 17:34:04 +0100

    fixed powerline

--- .vimrc
+++ .vimrc
@@ -28,6 +28,9 @@ nnoremap <C-K> <C-W><C-K>
 nnoremap <C-L> <C-W><C-L>
 nnoremap <C-H> <C-W><C-H>
 
+" remap folding keys
+nnoremap <F9> zA
+
 " set keymappings for tabs
 inoremap <Leader>m :tabnext<CR>
 noremap <Leader>n :tabprevious<CR>
--- templates/python.py
+++ templates/python.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import syslog
+""" Sample Script
+    Version 0.1
+"""
 
-class Debug:
-
-    def __init__(self, dlvl):
-        self.level = dlvl
-
-    def setLvl(self, dlvl):
-        self.level = dlvl
-
-    def stdout(self, dstr, dlvl=1):
-        if dlvl <= self.level:
-            print dstr
-
-    def syslog(self, dstr, dlvl=1):
-        if dlvl <= self.level:
-            syslog.syslog('%s: %s' % (sys.argv[0], dstr.replace('\n', ' ')))
+import logging
 
 def main():
-    debug = Debug(10)
-    debug.stdout('Debug string',1)
+    """ main program
+    """
+    logging.info('I am an informational log entry in the sample script.')
 
 if __name__ == '__main__':
     main()
