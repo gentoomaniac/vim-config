df34eb0ac645a41f67476499656bb1bb9aa663ab
Marco Siebecke <marco@spotify.com>
2014-09-29 15:36:43 +0200

    added global logger object to python template

--- templates/python.py
+++ templates/python.py
@@ -7,10 +7,15 @@
 
 import logging
 
+FORMAT = '%(asctime)-15s - %(message)s'
+logging.basicConfig(format=FORMAT)
+LOGGER = logging.getLogger('scriptlogger')
+LOGGER.setLevel(logging.INFO)
+
 def main():
     """ main program
     """
-    logging.info('I am an informational log entry in the sample script.')
+    LOGGER.info('I am an informational log entry in the sample script.')
 
 if __name__ == '__main__':
     main()
